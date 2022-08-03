c57ab4b0e0af48a9e58e6cc51af890d64f773199
Ilnar Gabdullin <ilnar30012000@gmail.com>
2022-08-03 20:27:38 +0500

    added autotest

---
+++ src/test/java/github/PracticeGitTest.java
@@ -0,0 +1,33 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeGitTest {
+    @BeforeAll
+    static void configure() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void searchTest() {
+        // - Откройте страницу Selenide в Github
+        open("https://github.com/");
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        // - Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+        // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".js-wiki-more-pages-link").click();
+        $(byText("SoftAssertions")).click();
+        // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+    }
+}
